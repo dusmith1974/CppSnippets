bdfa5912ec42cd0536943c5ab3e792a4162b09e3
duncan smith <dsmith1974@gmail.com>
2016-02-02 22:27:03 +0000

    Initial

---
+++ all/all/A.h
@@ -0,0 +1,15 @@
+#pragma once
+
+
+
+class A
+{
+public:
+	A();
+	explicit A(int iID);
+	A(int iID, const std::string& name);
+
+	A(const A& other);
+	A& operator=(const A other);
+
+};
---
+++ all/all/all.cpp
@@ -0,0 +1,11 @@
+// all.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+    return 0;
+}
+
